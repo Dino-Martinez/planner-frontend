52fd22ae54d11408ad4d6b8e07df055b6b75256a
Dino-Martinez <Dino.Martinez.514@gmail.com>
2021-07-15 12:52:21 -0700

    [update] style.css index.html scripts.js
    Adde filtering

--- scripts.js
+++ scripts.js
@@ -1,23 +1,50 @@
+
+// This code block to filter query string out of url was taken from 
+// https://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
+let urlParams
+(window.onpopstate = function () {
+    var match,
+        pl     = /\+/g,  // Regex for replacing addition symbol with a space
+        search = /([^&=]+)=?([^&]*)/g,
+        decode = function (s) { return decodeURIComponent(s.replace(pl, " ")) },
+        query  = window.location.search.substring(1)
+
+    urlParams = {}
+    while (match = search.exec(query))
+       urlParams[decode(match[1])] = decode(match[2])
+})()
+
+if (urlParams.filter === undefined) {
+    urlParams.filter = 'all'
+}
+
 const url = 'https://express-planner.herokuapp.com'
 const list = document.querySelector('#todo-list')
+const filter = document.querySelector('#filter')
+
+const currentFilterButton = document.querySelector(`#${urlParams.filter}-button`)
+currentFilterButton.classList.add('active-filter')
 
 async function populatePage() {
     const res = await fetch(url)
     const todos = await res.json()
     let itemsLeft = 0
     todos.forEach((todo, index) => {
-        if (!todo.completed) {
-            itemsLeft++
+        if ((urlParams.filter === 'all') || (urlParams.filter === 'active' && !todo.completed) || (urlParams.filter === 'completed' && todo.completed)) {
+            if (!todo.completed) {
+                itemsLeft++
+            }
+    
+            const li = document.createElement('li')
+            li.classList.add('todo')
+            li.innerHTML = `
+                <svg id="complete-${todo._id}" class="check-mark check-${todo.completed}" xmlns="http://www.w3.org/2000/svg" width="11" height="9"><path fill="none" stroke="#FFF" stroke-width="2" d="M1 4.304L3.696 7l6-6"/></svg>
+                <p class="todo-content status-${todo.completed}">${todo.title} </p>
+                <p style="display: none" id="status-${todo._id}">${todo.completed}</p>
+                <svg id="delete-${todo._id}" class="cross" xmlns="http://www.w3.org/2000/svg" width="18" height="18"><path fill="#494C6B" fill-rule="evenodd" d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z"/></svg>
+            `
+            list.appendChild(li)
         }
-        const li = document.createElement('li')
-        li.classList.add('todo')
-        li.innerHTML = `
-            <svg id="complete-${todo._id}" class="check-mark check-${todo.completed}" xmlns="http://www.w3.org/2000/svg" width="11" height="9"><path fill="none" stroke="#FFF" stroke-width="2" d="M1 4.304L3.696 7l6-6"/></svg>
-            <p class="todo-content status-${todo.completed}">${todo.title} </p>
-            <p style="display: none;" id="status-${todo._id}">${todo.completed}</p>
-            <svg id="delete-${todo._id}" class="cross" xmlns="http://www.w3.org/2000/svg" width="18" height="18"><path fill="#494C6B" fill-rule="evenodd" d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z"/></svg>
-        `
-        list.appendChild(li)
     })
 
     document.querySelector('#items-left').innerHTML = `${itemsLeft} items left`
@@ -45,8 +72,7 @@ function enableControls() {
                             body: 'No Body Implementation yet'
                         }) 
                       })
-                    input.value = ''
-                    window.location.reload()
+                    refresh()
                 }
             } else {
                 // Update todo
@@ -64,7 +90,7 @@ function enableControls() {
                         completed: status
                     }) 
                   })
-                window.location.reload()
+                refresh()
             }
         }
     })
@@ -83,12 +109,21 @@ function enableControls() {
                     id: id
                 }) 
               })
-            
-            window.location.reload()
+            refresh()
         }
     })
-    
+}
 
+function changeFilter (query) {
+    filter.innerHTML = query
+    refresh()
+}
+
+function refresh () {
+    // window.location.href = `http://${window.location.host}${window.location.pathname}?filter=${filter.innerHTML}`
+    window.location.href = `file:///D:/Programming/express-planner/frontend/index.html?filter=${filter.innerHTML}`   
 }
 
 populatePage()
+
+
