d772e64225d6bb519fbbf0d744eba5e92abfd7dc
Dino-Martinez <Dino.Martinez.514@gmail.com>
2021-07-17 16:34:19 -0700

    Implemented deleting

--- react-version/src/components/Todo.jsx
+++ react-version/src/components/Todo.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 function Todo(props) {
     return (
         <li className="todo">
-            <svg id={`complete-${props._id}`} class={`check-mark check-${props.completed}`} xmlns="http://www.w3.org/2000/svg" width="11" height="9"><path fill="none" stroke="#000" stroke-width="2" d="M1 4.304L3.696 7l6-6"/></svg>
+            <svg id={`complete-${props._id}`} className={`check-mark check-${props.completed}`} xmlns="http://www.w3.org/2000/svg" width="11" height="9"><path fill="none" stroke="#000" strokeWidth="2" d="M1 4.304L3.696 7l6-6"/></svg>
             <p className={`todo-content status-${props.completed}`}>{props.title} </p>
             <p id={`status-${props._id}`}>{props.completed}</p>
-            <svg id={`delete-${props._id}`} class="cross" xmlns="http://www.w3.org/2000/svg" width="18" height="18"><path fill="#494C6B" fill-rule="evenodd" d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z"/></svg>
+            <svg id={`delete-${props._id}`} onClick={() => props.delete(props._id)} className="cross" xmlns="http://www.w3.org/2000/svg" width="18" height="18"><path fill="#494C6B" fillRule="evenodd" d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z"/></svg>
         </li>
     )
 }
--- react-version/src/components/TodoList.jsx
+++ react-version/src/components/TodoList.jsx
@@ -1,12 +1,59 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Todo from './Todo'
 
 function TodoList(props) {
+    const [data, setData] = useState(undefined)
+    const url = 'https://express-planner.herokuapp.com'
+
+    const getData = async() => {
+        const res = await fetch(url)
+        const todos = await res.json()
+        todos.sort((a,b) => {
+            if (a.completed && b.completed)
+                return 0
+            if (!a.completed && b.completed)
+                return 1
+            if (a.completed && !b.completed)
+                return -1
+            return 0
+        })
+        setData(todos)
+    }
+
+    const deleteTodo = async (id) => {
+        await fetch(`${url}/${id}`, {
+            method: 'DELETE',
+            mode: 'cors',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                id: id
+            }) 
+          })
+    }
+
+    const toggleTodo = (id) => {
+
+    }
+
+    const createTodo = (id) => {
+    
+    }
+
+    useEffect(() => {
+        console.log('hi')
+        getData()
+    }, [])
+
     return (
         <ol className="todo-list">
-            {props.data.map(todo => {
-                return <Todo _id={todo._id} completed={todo.completed} title={todo.title}></Todo>
-            })}
+            {!data
+                ? <p>Loading...</p>
+                : data.map(todo => {
+                    return <Todo key={todo._id} _id={todo._id} completed={todo.completed} title={todo.title} delete={deleteTodo}></Todo>
+                })
+            }
         </ol>
     )
 }
