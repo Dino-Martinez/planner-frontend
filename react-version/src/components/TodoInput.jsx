9fc0c7e4ccab317c5719a5b891ec161a901d3fc3
Dino-Martinez <Dino.Martinez.514@gmail.com>
2021-07-17 20:27:13 -0700

    Implemented filtering, sorting, and styling

--- react-version/src/components/TodoInput.jsx
+++ react-version/src/components/TodoInput.jsx
@@ -5,7 +5,7 @@ function TodoInput(props) {
 
     return (
         <article id="create-todo" className="todo">
-            <svg onClick={()=>{ props.submit(value); setValue('')}}id="submit-create" className="check-mark submit-create" xmlns="http://www.w3.org/2000/svg" width="11" height="9"><path fill="none" stroke="#000" strokeWidth="2" d="M1 4.304L3.696 7l6-6"/></svg>
+            <svg onClick={()=>{ props.submit(value); setValue('')}}id="submit-create" className="check-mark submit-create" xmlns="http://www.w3.org/2000/svg" width="11" height="9"><path fill="none" stroke="#fff" strokeWidth="2" d="M1 4.304L3.696 7l6-6"/></svg>
             <input value={value} onChange={(e) => {setValue(e.target.value)}} id="todo-input" className="todo-content" type="text" name="create-todo" placeholder="Create new todo..." />
         </article>
     )
--- react-version/src/components/TodoList.jsx
+++ react-version/src/components/TodoList.jsx
@@ -2,22 +2,26 @@ import React, { useState, useEffect } from 'react'
 import Todo from './Todo'
 import TodoInput from './TodoInput'
 
+const compareTodos = (a,b) => {
+    if (a.completed && b.completed)
+        return 0
+    if (!a.completed && b.completed)
+        return 1
+    if (a.completed && !b.completed)
+        return -1
+    return 0
+}
+
 function TodoList(props) {
     const [data, setData] = useState(undefined)
+    const [filter, setFilter] = useState('all')
     const url = 'https://express-planner.herokuapp.com'
+    let numTodos = 0
 
     const getData = async() => {
         const res = await fetch(url)
         const todos = await res.json()
-        todos.sort((a,b) => {
-            if (a.completed && b.completed)
-                return 0
-            if (!a.completed && b.completed)
-                return 1
-            if (a.completed && !b.completed)
-                return -1
-            return 0
-        })
+        todos.sort(compareTodos)
 
         setData(todos)
     }
@@ -37,6 +41,24 @@ function TodoList(props) {
         setData(data.filter(todo => todo._id !== id))
     }
 
+    const deleteCompleted = () => {
+        const completed = data.filter(todo => todo.completed)
+        completed.forEach(async todo => {
+            await fetch(`${url}/${todo._id}`, {
+                method: 'DELETE',
+                mode: 'cors',
+                headers: {
+                  'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    id: todo._id
+                }) 
+              })
+        })
+        
+        setData(data.filter(todo => !todo.completed))
+    }
+
     const toggleTodo = async (id, completed) => {
         await fetch(`${url}/${id}`, {
             method: 'PUT',
@@ -45,7 +67,7 @@ function TodoList(props) {
               'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-                completed: completed
+                completed: !completed
             }) 
           })
 
@@ -57,7 +79,7 @@ function TodoList(props) {
                 todo.completed = !todo.completed
             }
             return todo
-        })
+        }).sort(compareTodos)
         setData(updatedItems)
     }
 
@@ -89,11 +111,28 @@ function TodoList(props) {
                     {!data
                         ? <p>Loading...</p>
                         : data.map(todo => {
-                            return <Todo key={todo._id} _id={todo._id} completed={todo.completed} title={todo.title} toggle={toggleTodo} delete={deleteTodo}></Todo>
+                            if (filter === 'all' || todo.completed === filter) {
+                                numTodos++
+                                return <Todo key={todo._id} _id={todo._id} completed={todo.completed} title={todo.title} toggle={toggleTodo} delete={deleteTodo}></Todo>
+                            }
+                            return ''
                         })
                     }
                 </ol>
+
+                <footer id="list-footer" className="flex-row list-footer">
+                    <p id="items-left">{numTodos} items left</p>
+                    <button id="all-button" onClick={() => setFilter('all')} className={`filter-button ${filter === 'all' ? 'active-filter' : ''}`}>All</button>
+                    <button id="active-button" onClick={() => setFilter(false)} className={`filter-button ${filter === false ? 'active-filter' : ''}`}>Active</button>
+                    <button id="completed-button" onClick={() => setFilter(true)} className={`filter-button ${filter === true ? 'active-filter' : ''}`}>Completed</button>
+                    <button id="clear-button" onClick={() => deleteCompleted()} className="clear-button">Clear Completed</button>
+                </footer>
             </main>
+            <aside className="mobile-filters">
+                <button id="mobile-all-button" onClick={() => setFilter('all')} className={`filter-button ${filter === 'all' ? 'active-filter' : ''}`}>All</button>
+                <button id="mobile-active-button" onClick={() => setFilter(false)} className={`filter-button ${filter === false ? 'active-filter' : ''}`}>Active</button>
+                <button id="mobile-completed-button" onClick={() => setFilter(true)} className={`filter-button ${filter === true ? 'active-filter' : ''}`}>Completed</button>
+            </aside>
         </React.Fragment>
     )
 }
